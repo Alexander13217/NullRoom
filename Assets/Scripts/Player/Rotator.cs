98a8a8e92b715443df2ce22c9437d4568f49a063
Papia1323 <146428652+Alexander13217@users.noreply.github.com>
2026-06-29 16:46:44 +0300

    feat: add Rotation system
    Added rotation system which rotates player and camera based on player input

---
+++ Assets/Scripts/Player/Rotator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float _sensitivity;
+    [SerializeField] private Camera _camera;
+
+    private readonly float _maxVerticalRotation = 70f;
+
+    private float _xRotation = 0f;
+    private float _yRotation = 0f;
+
+    private void Awake()
+    {
+        transform.rotation = Quaternion.identity;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        _xRotation += Input.GetAxis("Mouse X") * _sensitivity;
+        _yRotation += Input.GetAxis("Mouse Y") * _sensitivity;
+
+        transform.rotation = Quaternion.Euler(0f, _xRotation, 0f);
+        _yRotation = Mathf.Clamp(_yRotation, -_maxVerticalRotation, _maxVerticalRotation);
+        _camera.transform.rotation = Quaternion.Euler(-_yRotation, _xRotation, 0f);
+    }
+}
